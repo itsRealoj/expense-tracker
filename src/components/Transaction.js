43747811acacd9e07e45f6c1f06ba3c77cfe4e7d
Realoj <stephenojwang1040@gmail.com>
2020-05-31 11:03:55 +0300

    applied delete transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 export const Transaction = ({transaction}) => {
+    const { deletTransaction } = useContext(GlobalContext);
     const sign = transaction.amount < 0 ? '-' : '+';
 
         return (
             <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
                 {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-                <button className='delete-btn'>x</button>
+                <button onClick={() => deleteTransaction()} className='delete-btn'>x</button>
             </li>
         )
 }
