2f3f2b3d39136e39218c7c18c21e27e884962fbd
Realoj <stephenojwang1040@gmail.com>
2020-05-31 10:06:22 +0300

    aho

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+    return (
+        <>
+         <h3> Add New Transaction</h3>   
+         <form>
+             <div className='form-control'>
+                 <label htmlFor='text'>Text</label>
+                 <input type='text' placeholder='Enter text...'/>
+             </div>
+             <div className='form-control'>
+                 <label htmlFor='amount'>Amount <br /> (negative - expense, positive - income)</label>
+                 <input type='number' placeholder='Enter amount ..'/>
+             </div>
+             <button>Add transaction</button>
+         </form>
+        </>
+    )
+}
