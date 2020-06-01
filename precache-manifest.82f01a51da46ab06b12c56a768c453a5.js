b7a42469f44594f08bb7f6221969de4b9b9fb612
Realoj <stephenojwang1040@gmail.com>
2020-06-01 19:58:33 +0300

    Updates

---
+++ precache-manifest.82f01a51da46ab06b12c56a768c453a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575cf1ba2cea78ef65c366a694c51feb",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "49b2b65030528836ba9f",
+    "url": "/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "8c363021454f36faf87c",
+    "url": "/expense-tracker/static/js/2.42547532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.42547532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b2b65030528836ba9f",
+    "url": "/expense-tracker/static/js/main.fd5c3cc5.chunk.js"
+  },
+  {
+    "revision": "a282252542a1f2839049",
+    "url": "/expense-tracker/static/js/runtime-main.56b3400f.js"
+  }
+]);
